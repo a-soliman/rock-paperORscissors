7aade393c87220b1974e59268e23732a79aa639d
Ahmed Soliman <ahmedanwar@mail.com>
2016-06-19 00:40:27 -0700

    defined the compuseChoice based on Math.random()

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 
+// defining the computer chice
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
